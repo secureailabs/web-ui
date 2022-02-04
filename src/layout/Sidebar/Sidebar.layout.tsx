a2142ef49c12ae35bb46553599a7c18dc5b15ded
Adam Jasniewicz <adam@databending.ca>
2022-02-03 20:25:52 -0500

    npm test

--- src/layout/Sidebar/Sidebar.layout.tsx
+++ src/layout/Sidebar/Sidebar.layout.tsx
@@ -5,7 +5,6 @@ import type TSidebarLayout from './Sidebar.types';
 import Sidebar from '@components/Sidebar';
 
 const SidebarLayout: React.FC<TSidebarLayout> = ({ primary, secondary }) => {
-  console.log('primary, ', primary);
   return (
     <div className="sidebar-layout">
       <Sidebar primary={primary} secondary={secondary} />
