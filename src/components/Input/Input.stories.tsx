28752cbc27105f26ad8f42c657f0a424a76aa64b
Adam Jasniewicz <adam@databending.ca>
2022-02-08 10:44:27 -0500

    fix: added register to input story

--- src/components/Input/Input.stories.tsx
+++ src/components/Input/Input.stories.tsx
@@ -1,19 +1,25 @@
-import React from 'react';
+import React, { ReactElement } from 'react';
 import { Story, Meta } from '@storybook/react';
 
 import Input from './Input.component';
 import TInput from './Input.types';
+
+import { useForm } from 'react-hook-form';
 
 export default {
   title: 'Components/Input',
   component: Input,
 } as Meta;
 
-const Template: Story<TInput> = (args) => <Input {...args} />;
+function InputForm(args: TInput): ReactElement {
+  const { handleSubmit, register, formState } = useForm();
+  return <Input {...args} register={register} />;
+}
+
+const Template: Story<TInput> = (args) => <InputForm {...args} />;
 
 export const Simple = Template.bind({});
 Simple.args = {
   label: 'test',
   placeholder: 'test',
-  register: () => {}
 };
