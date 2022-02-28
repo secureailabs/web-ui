98bd5200af62555371a111668f445d80f6486a5c
Adam Jasniewicz <adam@databending.ca>
2022-02-28 08:58:50 -0500

    fix: sidebar exact prop

--- src/components/Sidebar/Sidebar.component.tsx
+++ src/components/Sidebar/Sidebar.component.tsx
@@ -8,26 +8,14 @@ const Sidebar: React.FC<TSidebar> = ({ primary, secondary, logo }) => {
     <div className="sidebar">
       <img src={logo} className="sidebar__logo" />
       <div className="sidebar__primary">
-        {primary.map(({ text, Icon, link, onClick }, index) => (
-          <SidebarLink
-            text={text}
-            Icon={Icon}
-            link={link}
-            onClick={onClick}
-            key={index}
-          />
+        {primary.map((props, index) => (
+          <SidebarLink {...props} key={index} />
         ))}
       </div>
 
       <div className="sidebar__secondary">
-        {secondary.map(({ text, Icon, link, onClick }, index) => (
-          <SidebarLink
-            text={text}
-            Icon={Icon}
-            link={link}
-            onClick={onClick}
-            key={index}
-          />
+        {secondary.map((props, index) => (
+          <SidebarLink {...props} key={index} />
         ))}
       </div>
     </div>
