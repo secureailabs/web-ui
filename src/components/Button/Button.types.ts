5cc8a5c7798f1a043f600e8e050757bd57c597d0
Prawal Gangwar <prawal.gangwar@gmail.com>
2022-07-26 18:19:36 -0400

    button on click event type

--- src/components/Button/Button.types.ts
+++ src/components/Button/Button.types.ts
@@ -2,7 +2,7 @@ type TButton = {
   children: string;
   button_type: 'primary' | 'secondary';
   type?: 'button' | 'submit' | 'reset';
-  onClick?: () => void;
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
   full: boolean;
   height?: '3.6rem' | '5rem';
   padded?: boolean;
